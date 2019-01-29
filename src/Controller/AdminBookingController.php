74100918fbe2e4302336bcc325ac2825e5630571
Manon <baillet.manon@gmail.com>
2019-01-29 17:56:28 +0100

    Admin réservations

---
+++ src/Controller/AdminBookingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BookingRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminBookingController extends AbstractController
+{
+    /**
+     * @Route("/admin/bookings", name="admin_bookings_index")
+     * @param BookingRepository $repo
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(BookingRepository $repo)
+    {
+
+
+        return $this->render('admin/booking/index.html.twig', [
+            'bookings' => $repo->findAll()
+        ]);
+    }
+}
