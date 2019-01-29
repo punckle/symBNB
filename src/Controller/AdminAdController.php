802b99f6af0183410cc80785494db8dd7eba43fe
Manon <baillet.manon@gmail.com>
2019-01-29 17:32:53 +0100

    Admin commentaires

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -51,4 +51,31 @@ class AdminAdController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     * @param Ad $ad
+     * @param ObjectManager $manager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/admin/ads/{id}/delete", name="admin_ads_delete")
+     */
+    public function delete(Ad $ad, ObjectManager $manager)
+    {
+        //On vérifie si l'annonce n'a pas de réservations en cours:
+        if (count($ad->getBookings()) > 0) {
+            $this->addFlash(
+                'warning',
+                "Vous ne pouvez pas supprimer l'annonce {$ad->getTitle()} car elle a des réservations en cours"
+            );
+        } else {
+            $manager->remove($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce {$ad->getTitle()} a bien été supprimée !"
+            );
+        }
+
+        return $this->redirectToRoute('admin_ads_index');
+    }
 }
---
+++ src/Controller/AdminCommentController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Form\AdminCommentType;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminCommentController extends AbstractController
+{
+    /**
+     * @Route("/admin/comments", name="admin_comments_index")
+     * @param CommentRepository $repo
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(CommentRepository $repo)
+    {
+        return $this->render('admin/comment/index.html.twig', [
+            'comments' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/comments/{id}/edit", name="admin_comments_edit")
+     * @param Comment $comment
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function edit(Comment $comment, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createForm(AdminCommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Le commentaire a bien été modifié !"
+            );
+        }
+
+        return $this->render('admin/comment/edit.html.twig', [
+            'comment' => $comment,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/comments/{id}/delete", name="admin_comments_delete")
+     * @param ObjectManager $manager
+     * @param Comment $comment
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function delete(ObjectManager $manager, Comment $comment)
+    {
+        $manager->remove($comment);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "Le commentaire a bien été supprimé !"
+        );
+
+        return $this->redirectToRoute('admin_comments_index');
+    }
+}
